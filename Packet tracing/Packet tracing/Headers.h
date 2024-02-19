ab59d0f6f1ce60f6b2c6dc777ce34e2f0dce87d4
Zoli068 <lackozoli1@gmail.com>
2024-02-19 15:09:09 +0100

    Progress
    Now the app working, just need some fix

--- Packet tracing/Packet tracing/Headers.h
+++ Packet tracing/Packet tracing/Headers.h
@@ -1,3 +1,20 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <chrono>
+#include "conio.h"
+#include <process.h>						
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
 struct ICMPheader
 {
     unsigned char    byType;
@@ -11,3 +28,5 @@ unsigned short checksum(unsigned short* buffer, int len);
 void print_raw_data(unsigned char* data, int br);
 void shutdown(SOCKET* socket);
 int packet_tracing();
+void changing_parameters();
+int get_the_ip();
--- Packet tracing/Packet tracing/packet-tracing.cpp
+++ Packet tracing/Packet tracing/packet-tracing.cpp
@@ -1,45 +1,57 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <chrono>
-#include "conio.h"
 #include "Headers.h"
-#include <process.h>						
-
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
 
 int max_ttl = 30;
 int attempts_per_router = 3;
 int showing_icmp_hex = 0;
+
 //Destination for the packet
 sockaddr_in dstAddr;
+
+
 int main() {
-	dstAddr.sin_addr.s_addr = inet_addr("151.101.0.81");
+	int choice;
+	int app_end = 1;
 
-	while (true) {
-
+	while (app_end) {
+		choice = -1;
+		system("CLS");
 		printf("*******************APP FOR TRACING THE PACKETS*******************\n\n");
-
 		printf("Available options:\n");
 		printf("1) Tracing a packet\n");
 		printf("2) Changing parameters\n");
-		printf("1-Changing the max TTL value\n");
-		printf("2-Changing the max attempts per router\n");
-		packet_tracing();
+		printf("3) Exit\n");
+		printf("\nEnter your choice: ");
 
+		scanf_s("%d",&choice);
 
-		printf("\n*****************************************************************");
-		break;
+		system("CLS");
+		switch (choice) {
+		case 1:
+
+			if (get_the_ip()) {
+				system("CLS");
+				packet_tracing();
+				system("CLS");
+			}break;
+		case 2:
+			changing_parameters();
+			break;
+
+		case 3:
+			app_end = 0;
+			break;
+		}
+
 	}
 
+	system("CLS");
+
+	printf("Thanks for using the app!\n");
+	printf("Author: Zoltan Lacko\n");
+	printf("Github: Zoli068");
+
+	printf("\n\nHit the enter to close the app");
+	_getch();
 	return 0;
 }
 
@@ -50,10 +62,14 @@ int packet_tracing()
 
 	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+
 	if (iResult != 0)
 	{
+		printf("\n\n");
 		printf("WSAStartup failed with error: %d\n", iResult);
-		return 1;
+		printf("\nHit the enter to continue");
+		_getch();
+		return -1;
 	}
 
 	dstAddr.sin_family = AF_INET;
@@ -75,6 +91,7 @@ int packet_tracing()
 	SOCKET sndSocket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
 
 	if (sndSocket == INVALID_SOCKET) {
+		printf("\n\n");
 		printf("Error with creating the socket with error:%d\n", WSAGetLastError());
 		shutdown(&sndSocket);
 		return -1;
@@ -85,21 +102,25 @@ int packet_tracing()
 	iResult = ioctlsocket(sndSocket, FIONBIO, &mode);
 
 	if (iResult != NO_ERROR) {
+		printf("\n\n");
 		printf("Error with changing to non-blocking socket with error: %ld\n", iResult);
 		shutdown(&sndSocket);
 		return -1;
 	}
 
 	if (showing_icmp_hex != 0) {
-		printf("Our icmp packet in hex format:");
+		printf("Our icmp packet in hex format:\n");
+		printf("------------------------------------");
 		print_raw_data((unsigned char*)&icmp_packet, sizeof(ICMPheader));
+		printf("\n------------------------------------\n");
+
 	}
 
 	char curr_ttl = 0;							
 	int sockadrr_size = sizeof(sockaddr_in);
 	std::chrono::high_resolution_clock::time_point start, end;
 	ICMPheader* rcvICMP = NULL;
-	int num_of_attempt;
+	int num_of_attempt;		//228.205.108.1
 	int selectResult;
 
 	//Creating a fd_set for the socket
@@ -111,16 +132,17 @@ int packet_tracing()
 	timeval timeVal;					//So,not the best, but will be fairly precise
 	timeVal.tv_sec = 1;
 	timeVal.tv_usec = 0;
-
-	printf("\nThe route for the packet is:\n");
-	printf("\n---------------------------------\n");
+	printf("Our destination is: %s", inet_ntoa(dstAddr.sin_addr));
+	printf("\n------------------------------------\n");
+	printf("The route for the packet is:");
+	printf("\n------------------------------------\n");
 	printf(" \tRTT\tIP address");
-	printf("\n---------------------------------\n");
+	printf("\n------------------------------------");
 
 	sockaddr_in routerAddress;
 	char rcvBuffer[256];
-
-	for (int i = 0;i < max_ttl;i++) {
+	int i;
+	for(i = 0;i < max_ttl;i++) {
 
 		//routerAddress will be set from recvfrom, and will containe the router IP where TTL went 0
 
@@ -143,6 +165,7 @@ int packet_tracing()
 			start = std::chrono::high_resolution_clock::now();
 
 			if (iResult == SOCKET_ERROR) {
+				printf("\n\n");
 				printf("Error at sending the packets with error:%d\n", WSAGetLastError());
 				shutdown(&sndSocket);
 				return -1;
@@ -159,6 +182,7 @@ int packet_tracing()
 			}
 			else if (selectResult == SOCKET_ERROR) {
 
+				printf("\n\n");
 				printf("Socket select failed!\n");
 				printf("%d", WSAGetLastError());
 				shutdown(&sndSocket);
@@ -178,6 +202,8 @@ int packet_tracing()
 				else
 				{
 					if (WSAGetLastError() != WSAEWOULDBLOCK) {
+
+						printf("\n\n");
 						printf("Reciving the packets failed with error: %d\n", WSAGetLastError());
 						shutdown(&sndSocket);
 						return -1;
@@ -209,7 +235,14 @@ int packet_tracing()
 		}
 
 	}
-	printf("\n---------------------------------");
+	printf("\n------------------------------------");
+	if(i>=max_ttl){
+		printf("\nCouldn't reach the destination\n\n");
+	}
+	else {
+		printf("\nSuccessfully reached the destination\n\n");
+	}
+
 	shutdown(&sndSocket);
 }
 
@@ -223,6 +256,8 @@ void shutdown(SOCKET* socket) {
 		printf("WSACleanup failed with error: %d\n", WSAGetLastError());
 	}
 
+	printf("\nHit the enter to continue");
+	_getch();
 }
 
 unsigned short checksum(unsigned short* buffer, int len)
@@ -248,7 +283,6 @@ unsigned short checksum(unsigned short* buffer, int len)
 	return (unsigned short)~sum;										//We returning the one's compilment, and bcs of that we using ~
 }
 
-
 void print_raw_data(unsigned char* data, int br) {						//For developing purpose
 
 	int i = 0;
@@ -260,5 +294,214 @@ void print_raw_data(unsigned char* data, int br) {						//For developing purpose
 		}
 
 		printf("%02X ", *(data + i));
+	}
+}
+
+int get_the_ip() {
+
+	WSADATA wsaData;
+
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (iResult != 0)
+	{
+		printf("\n\n");
+		printf("WSAStartup failed with error: %d\n", iResult);
+		printf("\nHit the enter to continue");
+		_getch();
+		return 0;
+	}
+
+	int choice = 0;
+	int loop_end = 1;
+	int i;
+
+	//aaa.bbb.ccc.ddd
+	char address[16];
+
+	struct hostent* myHostent;
+
+	//Found on the net,that max is 253
+	char domain_name[254];
+
+	memset(address, 0, 16);
+	memset(domain_name, 0, 254);
+
+	while (loop_end) {
+
+		printf("*******************GETTING THE DESTINATION******************\n\n");
+		printf("Available options:\n");
+		printf("1) Enter the IP address\n");
+		printf("2) Enter the domain name\n");
+		printf("3) Exit\n");
+		printf("\nEnter your choice: ");
+
+		scanf_s("%d", &choice);
+
+		system("CLS");
+		switch (choice) {
+		case 1:
+			printf("*******************GETTING THE IP ADDRESS******************\n\n");
+			printf("Enter the IP address: ");
+
+			scanf_s("%s", address, 16);
+			address[15] = '/0';
+			dstAddr.sin_addr.s_addr = inet_addr(address);
+
+			if (dstAddr.sin_addr.s_addr == INADDR_NONE) {
+
+				printf("\nInvalid IP address\n");
+
+				printf("\nHit the enter to continue");
+				_getch();
+
+				WSACleanup();
+				return 0;
+			}
+			else {
+				WSACleanup();
+				return 1;
+			}
+
+			break;
+
+		case 2:
+			printf("*******************GETTING THE DOMAIN NAME******************\n\n");
+			printf("Enter the domain name: ");
+
+			scanf_s("%s",domain_name,254);
+			domain_name[253] = '/0';
+
+
+
+			myHostent = gethostbyname(domain_name);
+
+			 if(myHostent ==NULL){
+
+				printf("\nInvalid domain name\n");
+				printf("\nHit the enter to continue");
+				_getch();
+
+				WSACleanup();
+				return 0;
+			}
+			else {
+
+				if (myHostent->h_addrtype == AF_INET) {			
+
+					inet_ntop(AF_INET, myHostent->h_addr_list[0], address, 16);
+
+					address[15] = '\0';
+
+					for (i = 0; myHostent->h_addr_list[i] != NULL; i++);
+
+
+					dstAddr.sin_addr.s_addr = inet_addr(address);
+
+
+					WSACleanup();
+					return 1;
+				}
+				else {
+					printf("\n This app works with IPv4 addresses only\n");
+					printf("\nHit the enter to continue");
+					_getch();
+
+
+					WSACleanup();
+					return 0;
+				}
+
+				WSACleanup();
+				return 0;
+			}
+
+			break;
+
+		case 3:
+
+			loop_end = 0;
+			break;
+		}
+		system("CLS");
+	}
+
+	WSACleanup();						//NO need for multiply setup neither cleanup
+	return 0;
+}
+
+void changing_parameters() {
+
+	int loop_end = 1;
+	int choice = 0;
+
+	while (loop_end) {
+		system("CLS");
+		printf("*******************CHANGING PARAMETERS*******************\n\n");
+
+		printf("Current values:\n");
+		printf("MAX TTL:  %d\n", max_ttl);
+		printf("MAX ATTEMPTS PER ROUTER:  %d\n", attempts_per_router);
+		printf("SHOWING ICMP IN HEX FORMAT :%s\n\n", (showing_icmp_hex) ? "SHOW" : "HIDE");
+		
+		printf("Available options:\n");
+		printf("1) Changing the max TTL value\n");
+		printf("2) Changing the max attempts per router\n");
+		printf("3) Showing ICMP packet that we using in hex format\n");
+		printf("4) Finish with the changing\n");
+		printf("\nEnter your choice: ");
+
+		scanf_s("%d", &choice);
+
+		system("CLS");
+		switch (choice) {
+		case 1:
+			printf("*******************CHANGING THE MAX TTL VALUE*******************\n\n");
+
+			printf("Enter the new max TTL value (1-255): ");
+
+			scanf_s("%d", &choice);
+
+			if (choice > 0 && choice < 255) {
+				max_ttl = choice;
+			}
+
+			break;
+
+		case 2:
+
+			printf("*******************CHANGING THE MAX ATTEMPTS PER ROUTER*******************\n\n");
+
+			printf("Enter the new value for the max attempts per router (1-10): ");
+
+			scanf_s("%d", &choice);
+
+			if (choice > 0 && choice < 10) {
+				attempts_per_router = choice;
+			}
+
+			break;
+
+		case 3:
+			printf("*******************SHOWING ICMP PACKET IN HEX FORMAT*******************\n\n");
+
+			printf("Option: 0-Hide");
+			printf("\n        1-Show");
+
+			printf("\n\nEnter your choice: ");
+			scanf_s("%d", &choice);
+
+			if (choice == 0 || choice == 1) {
+				showing_icmp_hex = choice;
+			}
+
+			break;
+
+		case 4:
+
+
+			loop_end = 0;
+			break;
+		}
 	}
 }
